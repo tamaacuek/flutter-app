a3b94f198b2c532d791a20520351c9cc4c8c822e
tamaacuek <ociganteng81@gmail.com>
2024-11-24 20:36:46 +0700

    memberikan user detail

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -27,6 +27,55 @@ class _UserPageState extends State<UserPage> {
     }
   }
 
+  void showUserDetail(UserModel user) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          title: const Text(
+            'User Details',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipOval(
+                child: Image.network(
+                  user.avatar ?? '-',
+                  width: 80.0,
+                  height: 80.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                '${user.firstName} ${user.lastName}',
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                user.email ?? '-',
+                style: const TextStyle(fontSize: 14.0),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,6 +118,7 @@ class _UserPageState extends State<UserPage> {
                   fontSize: 12.0,
                 ),
               ),
+              onTap: () => showUserDetail(user), // Tambahkan onTap
             );
           },
           separatorBuilder: (context, index) => const Divider(),
--- lib/util/user_service.dart
+++ lib/util/user_service.dart
@@ -1,35 +1,5 @@
 import 'package:appflutter/model/user_model.dart';
 import 'package:dio/dio.dart';
-
-// class UserService {
-//   final Dio _dio = Dio();
-//   static const _baseUrl = 'https://reqres.in/api';
-//   static const _users = 'users';
-
-//   Future<List<UserModel>> fetchUser() async {
-//     try {
-//       final response = await _dio.get(
-//         '$_baseUrl/$_users',
-//         queryParameters: {
-//           'page': '1',
-//         },
-//       );
-
-//       if (response.statusCode == 200 || response.statusCode == 201) {
-//         final data = response.data['data'];
-//         List<UserModel> users = List<UserModel>.from(
-//           data.map((user) => UserModel.fromJson(json: user)),
-//         );
-//         return users;
-//       } else {
-//         throw Exception('Failed to load users');
-//       }
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-// }
-
 class UserService {
   static final Dio _dio = Dio(); // Jadikan _dio juga statik jika perlu
   static const _baseUrl = 'https://reqres.in/api';
